4c2128fdfc708f81695cf2f018a677cd735a524c
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-08-05 15:33:48 +0900

    Delete script.js

--- 워드프레스/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var showcase = $('#showcase')
-    var playBtn = $('.play_button_image')
-    $(playBtn).click(function(){
-        $(showcase).css({'z-index':1})
-    });
-});
