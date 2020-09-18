022fa25ea4db5dd303ec23e2b38463b9771a3c5b
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-09-18 18:47:08 +0900

    Update script.js

--- kfcin/js/script.js
+++ kfcin/js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    
-  $(window).on("load", function (event) {
-  $(".preloader").delay(500).fadeOut(500);
-  });
-    
     var showcase = $('#showcase')
     var playBtn = $('.play_button_image')
     $(playBtn).click(function(){
-        $(showcase).css({'z-index':1})
+        $(this).css({"background-image":"none"}).html("loading...");
+        setTimeout(function(){
+            $(showcase).css({'z-index':1});
+        },3000)
     });
 });
