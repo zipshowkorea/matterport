ce70bf4192475b80c2de20d36210414cf4cecc25
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-09-23 10:46:04 +0900

    Update logo_change.js

--- chaser/logo_change.js
+++ chaser/logo_change.js
@@ -1,1 +1,1 @@
-$("#nhr #footer #footer-terms span.icon-matterport-brand").html("<img sr="https://zipshowkorea.github.io/matterport/kfcin/zipshow_logo.png" alt='집쇼코리아 로고'>);
+$("#nhr #footer #footer-terms span.icon-matterport-brand").html("<img sr="https://zipshowkorea.github.io/matterport/kfcin/zipshow_logo.png" alt='집쇼코리아 로고'>");
