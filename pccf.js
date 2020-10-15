43311db20bf1c1305f8677e403a05ceef28dd788
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-10-15 15:50:56 +0900

    Delete pccf.js

--- pccf.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var pccfLogo = $('#main-title #logo');
-    
-    var sns = $('.fa-external-link-alt:before')
-    sns.html(' ');
-    sns.css({'background':'https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg'});
-    var instagram = $('#summary');
-    instagram.children('a').html('@pccf_2020');
-});
