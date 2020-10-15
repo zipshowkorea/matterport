1e6db9c14e39644f4e3a8ee051cbdec1176aabb3
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-10-15 15:22:04 +0900

    Add files via upload

--- pccf.js
+++ pccf.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var pccfLogo = $('#main-title #logo');
     
-    var sns = $('.fa-external-link-alt')
+    var sns = $('.fa-external-link-alt:before')
     sns.html(' ');
     sns.css({'background':'https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg'});
     var instagram = $('#summary') ;
