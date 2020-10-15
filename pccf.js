ef4dd81d15581b60b30e75c54cbb577117dde3d8
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-10-15 15:17:28 +0900

    Update pccf.js

--- pccf.js
+++ pccf.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     
     var sns = $('.fa-external-link-alt')
     sns.html(' ');
-    sns.css('background':'https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg');
+    sns.css({'background':'https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg'});
     var instagram = $('#summary') ;
     instagram.children('a').html('@pccf_2020');
 });
