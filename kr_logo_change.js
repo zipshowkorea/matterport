f3e9ea3b901c667fd13f8d9b2e2ae5f962f4cd05
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2020-11-11 08:46:46 +0900

    Add files via upload

---
+++ kr_logo_change.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('src','https://zipshowkorea.github.io/matterport/kr_long_logo.png');
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('alt','집쇼코리아 로고');
+});
