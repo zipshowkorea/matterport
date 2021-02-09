5941f8915419b177d9c9f781f27ad3bc92a08aab
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2021-02-09 10:34:21 +0900

    Add files via upload

---
+++ chageLOGOjs/plant.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('src','https://zipshowkorea.com/d2/img/plant_square_logo.png');
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('alt','집쇼코리아 로고');
+});
