2f6001a0d1069469f89cbcae0a3cd4a9d9872de9
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2021-02-17 17:54:22 +0900

    Update plant.js

--- chageLOGOjs/plant.js
+++ chageLOGOjs/plant.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('src','https://zipshowkorea.com/d2/img/plant_square_logo.png').css({'width':'80px'});
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('src','').css({'width':'80px'});
     $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('alt','플랜트 로고');
 });
