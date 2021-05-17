8cb40f332cd73422d6c694b6334bf2c983b1cad3
zipshowkorea <68843907+zipshowkorea@users.noreply.github.com>
2021-05-17 14:13:46 +0900

    Add files via upload

---
+++ fairmont_seoul/fairmont_seoul.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('src','https://zipshowkorea.github.io/matterport/fairmont_seoul/Fairmont_seoul_ Black.png');
+    $("#nhr #footer #footer-terms span.icon-matterport-brand img").attr('alt','페어몬트포텔 로고');
+});
